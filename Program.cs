84373b6196d13d66e0fabc0f737b8f4eff4c0408
kzlager <highcolour@yandex.ru>
2022-02-13 18:29:12 +0300

    add files

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 14: Найти третью цифру числа или сообщить, что её нет.
+int GetRandom()
+{
+    int number = new Random().Next(0,200);
+    return number;
+}
+int randomazer = GetRandom();
+Console.WriteLine($"Случайное число: {randomazer}");
+int result = randomazer / 10;
+if(result >= 10)
+{
+    Console.WriteLine(randomazer % 10);
+}
+else
+{
+    Console.WriteLine("Нет третьей цифры");
+}
